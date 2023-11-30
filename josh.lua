ba5222a3bac073a2e0444a3297c78bd8231ddee0
piosterk <147023318+piosterk@users.noreply.github.com>
2023-11-30 13:32:48 +0800

    Create josh.lua

---
+++ josh.lua
@@ -0,0 +1,31 @@
+local Joshy = Instance.new("ScreenGui")
+Joshy.Name = "Joshy"
+Joshy.ResetOnSpawn = false
+Joshy.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
+Joshy.IgnoreGuiInset = true
+
+local Image = Instance.new("ImageLabel")
+Image.Name = "Image"
+Image.Size = UDim2.new(1, 0, 1, 0)
+Image.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Image.BackgroundTransparency = 1
+Image.BorderSizePixel = 0
+Image.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Image.Image = "rbxassetid://15502232408"
+Image.Parent = Joshy
+
+local Sound = Instance.new("Sound")
+Sound.RollOffMode = Enum.RollOffMode.Linear
+Sound.Volume = 2
+Sound.SoundId = "rbxassetid://15502249681"
+Sound.Parent = Joshy
+
+repeat task.wait() until Image.IsLoaded == true and Sound.IsLoaded == true
+
+Joshy.Parent = game.Players.LocalPlayer.PlayerGui
+
+Sound:Play()
+
+Sound.Ended:Once(function()
+	script.Parent:Destroy()
+end)
